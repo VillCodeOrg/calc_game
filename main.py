d540e0250227f9d99479bbbca21f3470acf8aa84
oren villaret <oren@itbits.blog>
2023-01-26 21:15:38 +0100

    fixed the logic

--- main.py
+++ main.py
@@ -11,42 +11,36 @@ def get_numbers():
     return first_number, second_number
 
 def get_operation():
-    operation_list = "-"
+    operation_list = ["-", "+"]
     return random.choice(operation_list)
 
-def check_operation():
-    if operation == "-":
-        meno(number_1, number_2, answer)
+def ask_question():
+    result = 0
+    answer = 0
+    number_1, number_2 = get_numbers()
+    while number_1 < number_2:
+        number_1, number_2 = get_numbers()
+    if get_operation() == "-":
+        result = number_1 - number_2
+        answer = int(input(f"{number_1} - {number_2} = "))
+    elif get_operation() == "+":
+        result = number_1 + number_2
+        answer = int(input(f"{number_1} + {number_2} = "))
+    check_answer(answer, result)
 
-def check_answer(result, answer):
-    if result == answer:
-        massege = "giusto"
+    
+def check_answer(answer, result):
+    if answer == result:
+        print("giusto")
     else:
-        massege = "sbagliato"
-
-
-def question():
-    answer = input(f"{number_1} - {number_2} = ")
-    check_operation()
-
-def meno(number_1, number_2, answer):
-    result = number_1 - number_2
-    check_answer(result, answer)
+        print("sbagliato")
 
 
 
 
-operation = get_operation()
-number_1 = get_numbers()[0]
-number_2 = get_numbers()[1]
-while number_1 <= number_2:
-    number_1 = get_operation[0]
-    number_2 = get_operation[1]
-answer = question()
 
 
 
-        
-
+ask_question()
 
 
